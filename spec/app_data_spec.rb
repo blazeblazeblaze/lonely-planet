14e4ae5a387b997845d12cbcf46179468a54e5ed
Blazej Hadzik <hadzikblazej@gmail.com>
2016-02-15 09:04:32 +1100

    App data to test end-to-end. Use real data.

--- spec/app_data_spec.rb
+++ spec/app_data_spec.rb
@@ -1,21 +1,44 @@
 require 'spec_helper'
 
 describe AppData do
+  let(:destination_xml ) { File.open("spec/sample/destinations.xml") }
+  let(:locations_xml )   { File.open("spec/sample/taxonomy.xml") }
+
   let(:struct) { Struct.new(:taxonomy_path, :destinations_path) }
-  let(:opts)   { struct.new(__FILE__, __FILE__) }
+  let(:opts)   { struct.new(locations_xml, destination_xml) }
 
   subject      { described_class.new(opts) }
 
+  let(:parsed_location) do
+    {
+      "355064" => { parent: nil, children: ["355611"] },
+      "355611" => { parent: "355064", children: [] }
+    }
+  end
+
+  let(:parsed_destinations) do
+    {
+      "355064" => {
+        atlas_id: "355064",
+        title: "Africa",
+        history: "You’ve probably heard the claim that Africa is ‘the birthplace of humanity’."
+      }
+    }
+  end
+
   describe "#load" do
-    before do
-      allow(subject).to receive(:taxonomies_nodes)   { Thread.new{} }
-      allow(subject).to receive(:destinations_nodes) { Thread.new{} }
+    before { subject.load }
+
+    it "returns parsed locations" do
+      expect(
+        subject.taxonomies
+      ).to eq(parsed_location)
     end
 
-    it "creates 2 threads and joins. returns array." do
+    it 'returns parsed destinations' do
       expect(
-        subject.load
-      ).to be_an_instance_of(Array)
+        subject.destinations
+      ).to eq(parsed_destinations)
     end
   end
 end
