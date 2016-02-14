7162861635f3e8fc9440821f43088737793e3946
Blazej Hadzik <hadzikblazej@gmail.com>
2016-02-15 09:03:24 +1100

    Add node parser class.

---
+++ lib/parser/node_parser.rb
@@ -0,0 +1,22 @@
+require_relative 'base_parser'
+
+class NodeParser < BaseParser
+  def parse
+    hashify_nodes([@handler.location_nodes])
+  end
+
+  private
+
+  def hashify_nodes(node_arr=[], parent=nil)
+    node_arr.each do |node|
+      first, *rest  = node
+
+      @nodes[first] = {
+        parent:  parent,
+        children: rest.map(&:first)
+      }
+
+      hashify_nodes(rest, first)
+    end
+  end
+end
