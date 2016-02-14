857eda2d3710d19c88c79f40a23cecf322556f0e
Blazej Hadzik <hadzikblazej@gmail.com>
2016-02-14 20:54:29 +1100

    Add destination view model class.

---
+++ lib/view_models/destination_page.rb
@@ -0,0 +1,48 @@
+module ViewModels
+  class DestinationPage
+    attr_reader :destination
+
+    def initialize(destination)
+      @destination = destination
+    end
+
+    def title
+      destination.title
+    end
+
+    def content
+      destination.history
+    end
+
+    def atlas_id
+      destination.atlas_id
+    end
+
+    def children
+      destination.children.map do |child|
+        nav_link(child)
+      end
+    end
+
+    def parents
+      navigation  = [nav_link(destination)]
+      parent      = destination.parent
+
+      while parent do
+        navigation << nav_link(parent)
+        parent = parent.parent
+      end
+
+      navigation.reverse
+    end
+
+    private
+
+    def nav_link(item)
+      {
+        title:    item.title,
+        atlas_id: item.atlas_id
+      }
+    end
+  end
+end
