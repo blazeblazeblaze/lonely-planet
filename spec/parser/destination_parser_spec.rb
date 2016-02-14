9082e072b1004b2dbff2dad10b5db18a08b5a7f4
Blazej Hadzik <hadzikblazej@gmail.com>
2016-02-15 09:03:36 +1100

    add destination parser spec. (ox based)

---
+++ spec/parser/destination_parser_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe DestinationParser do
+  let(:io)      { StringIO.new }
+  let(:handler) { double("Some handler") }
+  let(:subject) { described_class.new(xml: io, handler: handler) }
+
+  describe ".new" do
+    it "runs Ox parse on init" do
+      expect(Ox).to receive(:sax_parse).with(handler, io)
+      subject
+    end
+  end
+
+  describe '#parse' do
+    let(:node_item) { { atlas_id: 123 } }
+    before do
+      allow(handler).to receive(:destination_nodes) { [node_item] }
+    end
+
+    it 'hashifies xml' do
+      expect(handler).to receive(:destination_nodes)
+      subject.parse
+    end
+
+    it 'returns hashified xml' do
+      expect(
+        subject.parse
+      ).to eq({ 123 => node_item })
+    end
+  end
+end
