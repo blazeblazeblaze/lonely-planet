f617e30f29cb03e081d5fc86ee348858e702feac
Blazej Hadzik <hadzikblazej@gmail.com>
2016-02-15 14:18:40 +1100

    No need to use struct. Use hashes.

--- spec/renderer/destination_spec.rb
+++ spec/renderer/destination_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 describe Renderer::Destination do
-  let(:klass)       { Struct.new(:title, :atlas_id, :children, :parent) }
-  let(:destination) { klass.new("Costa Rica", 123) }
-  let(:child)       { klass.new("San Jose", 124) }
-  let(:parent)      { klass.new("America", 125) }
+  let(:destination) { { title: "Costa Rica", atlas_id: 123, children: 124 } }
+  let(:child)       { { title: "San Jose", atlas_id: 124, parent: 123 } }
 
   before do
+    allow(destination).to receive(:parent)   { nil }
     allow(destination).to receive(:children) { [child] }
-    allow(destination).to receive(:parent)   { parent }
     allow(destination).to receive(:history)  { "some history" }
   end
 
@@ -16,7 +14,7 @@ describe Renderer::Destination do
     it "renders destination html content" do
       expect(
         described_class.render(destination)
-      ).to include("Costa Rica", "San Jose", "America")
+      ).to include("Costa Rica", "San Jose")
     end
   end
 end
